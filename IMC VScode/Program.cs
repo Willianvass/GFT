3c91786222df9cd196787a3b51bdd84d537fc801
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-06-29 22:00:15 -0300

    Exercicio 1

---
+++ IMC VScode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace IMC
+{
+    public class Calculo_IMC
+    {
+        static void Main(string[] args)
+        {
+            int nascimento, idade;
+            double IMC, altura, peso;
+            string sair, opcoes;
+            System.Console.Write("Qual ano de nascimento: ");
+            nascimento = Int32.Parse(Console.ReadLine());
+            System.Console.Write("Qual sua altura: ");
+            altura = double.Parse(Console.ReadLine());
+            System.Console.Write("Qual seu peso: ");
+            peso = double.Parse(Console.ReadLine()); 
+
+            idade = 2022 - nascimento;
+            IMC = peso / (altura * altura);
+
+            do
+            {  
+                System.Console.WriteLine("Digite a opção desejada: \n1 - Idade; \n2 - IMC; \n3 - Sair!");
+                opcoes = Console.ReadLine();
+
+                switch (opcoes)
+                {
+                    case "1":
+                        System.Console.WriteLine("Sua idade é: " + idade);
+                        break;
+                    case "2":
+                        System.Console.WriteLine(("Seu IMC é: " + IMC.ToString("0.00")));
+                        break;
+                    case "3":
+                        System.Console.WriteLine("Até logo");
+                        break;
+                }
+            } while (opcoes != "3");
+        }
+    }
+}
